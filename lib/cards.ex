ea4884e1b8e2a7e8b8f1ffc649c4441f16b4f4c2
Marise St-Pierre <marise.stpierre@nventive.com>
2022-12-14 15:17:24 -0500

    add functions to cards module

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,15 @@
 defmodule Cards do
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 
 end

d4481c2e0f7ced023727ed566fa847d8d17bdb7f
Marise St-Pierre <marise.stpierre@nventive.com>
2022-12-23 11:59:12 -0500

    add documentation and tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -4,6 +4,9 @@ defmodule Cards do
     Provides methodes for creating and handling a deck of cards
   """
 
+  @doc """
+    Return a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -17,10 +20,28 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines wheter a deck contains a given card
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      iex> true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} =Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
